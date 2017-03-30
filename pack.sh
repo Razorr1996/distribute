78c3f9cd702c37ac0c9d27a6865c4e732556f55e
Razorr1996 <basa62.1996@gmail.com>
2017-03-30 23:38:38 +0300

    Script tar fix

--- pack.sh
+++ pack.sh
@@ -3,5 +3,7 @@ mkdir target
 mkdir target/pa1
 rm target/pa1/*
 cp src/*.c headers/*.h headers/*/*.h target/pa1
-tar -cvzf target/pa1.tar.gz target/pa1
-#clang -std=c99 -Wall -Werror -pedantic main.c
+cd target
+tar -cvzf pa1.tar.gz pa1
+echo "all:
+\tclang -std=c99 -Wall -Werror -pedantic main.c" > pa1/Makefile
