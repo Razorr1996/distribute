32e0278a950bb94f4d39b2a62d3f611d9e26a773
Razorr1996 <basa62.1996@gmail.com>
2017-04-10 00:26:42 +0300

    Receive and etc.

--- src/main.c
+++ src/main.c
@@ -16,6 +16,9 @@
 
 #define MAX_FORK_MAGIC 10
 
+static size_t sizeHeader = sizeof(MessageHeader);
+static size_t sizeMessage = sizeof(Message);
+
 typedef struct {
     int writePipe;
     int readPipe;
@@ -24,6 +27,8 @@ typedef struct {
 typedef struct {
     local_id localID;
     int nChild;
+    pid_t pid;
+    pid_t pPid;
     pipeDes pDes[MAX_PROCESS_ID + 1][MAX_PROCESS_ID + 1];
 } __attribute__((packed)) LocalInfo;
 
@@ -41,7 +46,8 @@ int send(void *self, local_id dst, const Message *msg) {
     LocalInfo *info = self;
     size_t n = sizeof(msg->s_header) + msg->s_header.s_payload_len;
     ssize_t res = write(info->pDes[info->localID][dst].writePipe, msg, n);
-    printf("From localID=%d to %d write %d\n", info->localID, dst, (int) res);
+    if (res != n) return EXIT_FAILURE;
+//    printf("From localID=%d to %d write %d\n", info->localID, dst, (int) res);
     return EXIT_SUCCESS;
 }
 
@@ -52,8 +58,25 @@ int send_multicast(void *self, const Message *msg) {
             size_t n = sizeof(msg->s_header) + msg->s_header.s_payload_len;
             ssize_t res = write(info->pDes[info->localID][i].writePipe, msg, n);
             if (res != n) return EXIT_FAILURE;
+//            printf("From localID=%d to %d write %d\n", info->localID, i, (int) res);
         }
     }
+    return EXIT_SUCCESS;
+}
+
+int receive(void *self, local_id from, Message *msg) {//TODO Do it
+    LocalInfo *info = self;
+    ssize_t res, res1;
+    do {
+        res = read(info->pDes[from][info->localID].readPipe, msg, sizeHeader);
+    } while (res == -1 && errno == EAGAIN);
+    do {
+        res1 = read(info->pDes[from][info->localID].readPipe, msg + sizeHeader, (size_t) res);
+    } while (res1 == -1 && errno == EAGAIN);
+    return EXIT_SUCCESS;
+}
+
+int receive_any(void *self, Message *msg) {//TODO Do it
     return EXIT_SUCCESS;
 }
 
@@ -61,12 +84,14 @@ int main(int argc, char *argv[]) {
     LocalInfo *info = malloc(sizeof(LocalInfo));
     info->localID = 0;
     info->nChild = 8;
+    info->pid = getpid();
+    info->pPid = getppid();
     memset(info->pDes, -1, sizeof(info->pDes));
     int opt, i;
     pid_t pid = 1;
     int eventFd = open(events_log, O_CREAT | O_WRONLY | O_APPEND);
     if (eventFd == -1) {
-        strerror(errno);
+        perror(strerror(errno));
         exit(EXIT_FAILURE);
     }
     opt = getopt(argc, argv, "p:");
@@ -78,11 +103,11 @@ int main(int argc, char *argv[]) {
         info->nChild = nChildTmp > 0 ? nChildTmp : info->nChild;
     }
     {//debug output
-        time_t rawtime;
-        struct tm *timeinfo;
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
-        logToFile(eventFd, "\nTime: %s", asctime(timeinfo));
+        time_t rawTime;
+        struct tm *timeInfo;
+        time(&rawTime);
+        timeInfo = localtime(&rawTime);
+        logToFile(eventFd, "\nTime: %s", asctime(timeInfo));
         logToFile(eventFd, "nChild=%d\n", info->nChild);
         logToFile(eventFd, "argc=%d\n", argc);
         for (i = 0; i < argc; ++i) {
@@ -109,21 +134,23 @@ int main(int argc, char *argv[]) {
         pid = fork();
         if (pid == 0) {
             info->localID = (local_id) i;
+            info->pPid = info->pid;
+            info->pid = getpid();
         }
 
     }
 
     Message myMsg;
-    memset(&myMsg, 0, sizeof(Message));
-    snprintf(myMsg.s_payload, MAX_PAYLOAD_LEN, log_started_fmt, info->localID, getpid(), getppid());
-    myMsg.s_header.s_payload_len = (uint16_t) strlen(myMsg.s_payload);
+    memset(&myMsg, 0, sizeMessage);
+    snprintf(myMsg.s_payload, MAX_PAYLOAD_LEN, log_started_fmt, info->localID, info->pid, info->pPid);
     myMsg.s_header.s_magic = MESSAGE_MAGIC;
+    myMsg.s_header.s_payload_len = (uint16_t) strlen(myMsg.s_payload);
+    myMsg.s_header.s_type = STARTED;
     myMsg.s_header.s_local_time = 0L;
-    myMsg.s_header.s_type = DONE;
 
-    logToFile(eventFd, log_started_fmt, info->localID, getpid(), getppid());
+    logToFile(eventFd, myMsg.s_payload);
 
-    send(info, (local_id) (info->localID + 1) % (info->nChild + 1), &myMsg);
+    send_multicast(info, &myMsg);
     //
     close(eventFd);
     return EXIT_SUCCESS;
