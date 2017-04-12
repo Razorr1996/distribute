26845c36d8a1b4a1f6e4036397996303e396853b
Razorr1996 <basa62.1996@gmail.com>
2017-04-12 18:38:17 +0300

    Passed all tests for pa1

--- pack.sh
+++ pack.sh
@@ -2,8 +2,8 @@
 mkdir target
 mkdir target/pa1
 rm target/pa1/*
-cp src/*.c headers/*.h headers/*/*.h target/pa1
+cp src/main.c headers/*.h headers/*/*.h target/pa1
 cd target
 tar -cvzf pa1.tar.gz pa1
 echo "all:
-\tclang -std=c99 -Wall -Werror -pedantic main.c" > pa1/Makefile
+\tclang -std=c99 -Wall -Werror -pedantic *.c" > pa1/Makefile
--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <errno.h>
+#include <wait.h>
 
 #include "common.h"
 #include "ipc.h"
@@ -16,20 +17,20 @@
 
 #define MAX_FORK_MAGIC 10
 
-static size_t sizeHeader = sizeof(MessageHeader);
-static size_t sizeMessage = sizeof(Message);
+static const size_t sizeHeader = sizeof(MessageHeader);
+static const size_t sizeMessage = sizeof(Message);
 
 typedef struct {
-    int writePipe;
     int readPipe;
-}__attribute__((packed)) pipeDes;
+    int writePipe;
+}__attribute__((packed)) PipeDes;
 
 typedef struct {
     local_id localID;
-    int nChild;
+    int nChild, eventFd, logFd;
     pid_t pid;
     pid_t pPid;
-    pipeDes pDes[MAX_PROCESS_ID + 1][MAX_PROCESS_ID + 1];
+    PipeDes pDes[MAX_PROCESS_ID + 1][MAX_PROCESS_ID + 1];
 } __attribute__((packed)) LocalInfo;
 
 int logToFile(int fd, const char *format, ...) {
@@ -42,12 +43,18 @@ int logToFile(int fd, const char *format, ...) {
     return EXIT_SUCCESS;
 }
 
+int exitWithError(int fd, int status) {
+    dprintf(fd, "ERROR: %s\n", strerror(status));
+    fprintf(stderr, "%s\n", strerror(status));
+    fflush(stderr);
+    exit(status);
+}
+
 int send(void *self, local_id dst, const Message *msg) {
     LocalInfo *info = self;
     size_t n = sizeof(msg->s_header) + msg->s_header.s_payload_len;
     ssize_t res = write(info->pDes[info->localID][dst].writePipe, msg, n);
     if (res != n) return EXIT_FAILURE;
-//    printf("From localID=%d to %d write %d\n", info->localID, dst, (int) res);
     return EXIT_SUCCESS;
 }
 
@@ -55,28 +62,124 @@ int send_multicast(void *self, const Message *msg) {
     LocalInfo *info = self;
     for (local_id i = 0; i <= info->nChild; ++i) {
         if (i != info->localID) {
-            size_t n = sizeof(msg->s_header) + msg->s_header.s_payload_len;
+            size_t n = sizeHeader + msg->s_header.s_payload_len;
             ssize_t res = write(info->pDes[info->localID][i].writePipe, msg, n);
-            if (res != n) return EXIT_FAILURE;
-//            printf("From localID=%d to %d write %d\n", info->localID, i, (int) res);
+            if (res != n && errno != EAGAIN) {
+                exitWithError(info->eventFd, errno);
+            }
         }
     }
     return EXIT_SUCCESS;
 }
 
-int receive(void *self, local_id from, Message *msg) {//TODO Do it
+int receive(void *self, local_id from, Message *msg) {
     LocalInfo *info = self;
-    ssize_t res, res1;
+    ssize_t res = 0;
+    size_t c1 = sizeHeader;
     do {
-        res = read(info->pDes[from][info->localID].readPipe, msg, sizeHeader);
-    } while (res == -1 && errno == EAGAIN);
+        if (res > 0) {
+            c1 -= res;
+        } else usleep(10000);
+        res = read(info->pDes[from][info->localID].readPipe, msg, c1);
+        if (res == -1 && errno != EAGAIN) {
+            exitWithError(info->eventFd, errno);
+        }
+    } while (c1 > 0);
+    c1 = msg->s_header.s_payload_len;
     do {
-        res1 = read(info->pDes[from][info->localID].readPipe, msg + sizeHeader, (size_t) res);
-    } while (res1 == -1 && errno == EAGAIN);
+        if (res > 0) {
+            c1 -= res;
+        } else usleep(10000);
+        res = read(info->pDes[from][info->localID].readPipe, &msg->s_payload, c1);
+        if (res == -1) exitWithError(info->eventFd, errno);
+    } while (c1 > 0);
     return EXIT_SUCCESS;
 }
 
-int receive_any(void *self, Message *msg) {//TODO Do it
+/*int receive_any(void *self, Message *msg) {//TODO To receive
+    LocalInfo *info = self;
+    ssize_t res, res1;
+    int count = 1;
+    int arrTmp[info->nChild + 1];
+    memset(arrTmp, 0, sizeof(arrTmp));
+    arrTmp[0] = 1;
+    for (int from = 1; count > 0; from = (from + 1) % info->nChild + 1) {
+        if (arrTmp[from]) {
+            count--;
+        }
+    }
+    return EXIT_SUCCESS;
+}*/
+
+int receiveAll(LocalInfo *info) {
+    Message inMsg;
+    memset(&inMsg, 0, sizeMessage);
+    for (int i = 1; i <= info->nChild; i++) {
+        if (i != info->localID) {
+            receive(info, i, &inMsg);
+            logToFile(info->eventFd, "Msg(%2d->%2d):\t%s", i, info->localID, inMsg.s_payload);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+int openPipes(LocalInfo *info) {
+    int fDes[2];
+    for (int i = 0; i <= info->nChild; ++i) {
+        for (int j = 0; j <= info->nChild; ++j) {
+            if (i != j) {
+                if (pipe(fDes)) {
+                    fprintf(stderr, "Cannot open pipe.\n");
+                    exit(EXIT_FAILURE);
+                } else {
+                    fcntl(fDes[0], F_SETFL, fcntl(fDes[1], F_GETFL) | O_NONBLOCK);
+                    info->pDes[i][j].readPipe = fDes[0];
+                    info->pDes[i][j].writePipe = fDes[1];
+                }
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+int closeUnnecessaryPipes(LocalInfo *info) {
+    logToFile(info->eventFd, "Process %2d start closing unnecessary pipes\n", info->localID);
+    for (int i = 0; i <= info->nChild; ++i) {
+        for (int j = 0; j <= info->nChild; ++j) {
+            if (i == j) continue;
+            if (i == info->localID) {
+                close(info->pDes[i][j].readPipe);
+                info->pDes[i][j].readPipe = -1;
+                continue;
+            }
+            if (j == info->localID) {
+                close(info->pDes[i][j].writePipe);
+                info->pDes[i][j].writePipe = -1;
+                continue;
+            } else {
+                close(info->pDes[i][j].readPipe);
+                close(info->pDes[i][j].writePipe);
+                info->pDes[i][j].readPipe = -1;
+                info->pDes[i][j].writePipe = -1;
+                continue;
+            }
+        }
+    }
+    logToFile(info->eventFd, "Process %2d end closing unnecessary pipes\n", info->localID);
+    return EXIT_SUCCESS;
+}
+
+int closeUsedPipes(LocalInfo *info) {
+    logToFile(info->eventFd, "Process %2d start closing used pipes\n", info->localID);
+    for (int i = 0; i <= info->nChild; ++i) {
+        if (i != info->localID) {
+            close(info->pDes[info->localID][i].writePipe);
+            close(info->pDes[i][info->localID].readPipe);
+            info->pDes[info->localID][i].writePipe = -1;
+            info->pDes[i][info->localID].readPipe = -1;
+        }
+    }
+    logToFile(info->eventFd, "Process %2d end closing used pipes\n", info->localID);
     return EXIT_SUCCESS;
 }
 
@@ -86,15 +189,12 @@ int main(int argc, char *argv[]) {
     info->nChild = 8;
     info->pid = getpid();
     info->pPid = getppid();
-    memset(info->pDes, -1, sizeof(info->pDes));
-    int opt, i;
-    pid_t pid = 1;
-    int eventFd = open(events_log, O_CREAT | O_WRONLY | O_APPEND);
-    if (eventFd == -1) {
-        perror(strerror(errno));
-        exit(EXIT_FAILURE);
+    info->eventFd = open(events_log, O_CREAT | O_WRONLY | O_APPEND);
+    if (info->eventFd == -1) {
+        exitWithError(info->eventFd, errno);
     }
-    opt = getopt(argc, argv, "p:");
+    memset(info->pDes, -1, sizeof(info->pDes));
+    int opt = getopt(argc, argv, "p:");
     if (opt != -1 && optarg != NULL) {
         int nChildTmp;
         sscanf(optarg, "%d", &nChildTmp);
@@ -107,51 +207,67 @@ int main(int argc, char *argv[]) {
         struct tm *timeInfo;
         time(&rawTime);
         timeInfo = localtime(&rawTime);
-        logToFile(eventFd, "\nTime: %s", asctime(timeInfo));
-        logToFile(eventFd, "nChild=%d\n", info->nChild);
-        logToFile(eventFd, "argc=%d\n", argc);
-        for (i = 0; i < argc; ++i) {
-            logToFile(eventFd, "argv[%d]=%s\n", i, argv[i]);
-        }
+        logToFile(info->eventFd, "\nTime: %s", asctime(timeInfo));
+        logToFile(info->eventFd, "Children=%d\n", info->nChild);
+        fsync(1);
+        fsync(info->eventFd);
     }
 
-    for (i = 0; i <= info->nChild; ++i) {
-        for (int j = 0; j <= info->nChild; ++j) {
-            int fDes[2];
-            if (i != j) {
-                if (pipe2(fDes, O_NONBLOCK)) {
-                    fprintf(stderr, "Cannot open pipe.\n");
-                    exit(EXIT_FAILURE);
-                } else {
-                    info->pDes[i][j].readPipe = fDes[0];
-                    info->pDes[i][j].writePipe = fDes[1];
-                }
-            }
-        }
-    }
+    if (openPipes(info) != EXIT_SUCCESS) exitWithError(info->eventFd, errno);
 
-    for (i = 1; i <= info->nChild && pid != 0; ++i) {
+    logToFile(info->eventFd, "Fork[0]: %5d\n", info->pid);
+    for (int i = 1, pid = 1; i <= info->nChild && pid != 0; ++i) {
         pid = fork();
         if (pid == 0) {
             info->localID = (local_id) i;
             info->pPid = info->pid;
             info->pid = getpid();
+        } else {
+            logToFile(info->eventFd, "Fork[%d]: %5d\n", i, pid);
         }
-
     }
 
-    Message myMsg;
-    memset(&myMsg, 0, sizeMessage);
-    snprintf(myMsg.s_payload, MAX_PAYLOAD_LEN, log_started_fmt, info->localID, info->pid, info->pPid);
-    myMsg.s_header.s_magic = MESSAGE_MAGIC;
-    myMsg.s_header.s_payload_len = (uint16_t) strlen(myMsg.s_payload);
-    myMsg.s_header.s_type = STARTED;
-    myMsg.s_header.s_local_time = 0L;
+    closeUnnecessaryPipes(info);
 
-    logToFile(eventFd, myMsg.s_payload);
+    if (info->localID != PARENT_ID) {
+        Message myMsg;
+        memset(&myMsg, 0, sizeMessage);
+        snprintf(myMsg.s_payload, MAX_PAYLOAD_LEN, log_started_fmt, info->localID, info->pid, info->pPid);
+        myMsg.s_header.s_magic = MESSAGE_MAGIC;
+        myMsg.s_header.s_payload_len = (uint16_t) strlen(myMsg.s_payload);
+        myMsg.s_header.s_type = STARTED;
+        myMsg.s_header.s_local_time = 0L;
 
-    send_multicast(info, &myMsg);
+        logToFile(info->eventFd, myMsg.s_payload);
+
+        send_multicast(info, &myMsg);
+    }
+
+    receiveAll(info);
+    logToFile(info->eventFd, log_received_all_started_fmt, info->localID);
+    if (info->localID != PARENT_ID) {
+        Message myMsg;
+        memset(&myMsg, 0, sizeMessage);
+        snprintf(myMsg.s_payload, MAX_PAYLOAD_LEN, log_done_fmt, info->localID);
+        myMsg.s_header.s_magic = MESSAGE_MAGIC;
+        myMsg.s_header.s_payload_len = (uint16_t) strlen(myMsg.s_payload);
+        myMsg.s_header.s_type = DONE;
+        myMsg.s_header.s_local_time = 0L;
+
+        logToFile(info->eventFd, myMsg.s_payload);
+
+        send_multicast(info, &myMsg);
+    }
+    receiveAll(info);
+    logToFile(info->eventFd, log_received_all_done_fmt, info->localID);
+    closeUsedPipes(info);
+    if (info->localID == PARENT_ID) {
+        for (int j = 0; j < info->nChild; ++j) {
+            wait(NULL);
+        }
+        logToFile(info->eventFd, "All children end\n");
+    }
     //
-    close(eventFd);
+    close(info->eventFd);
     return EXIT_SUCCESS;
 }
