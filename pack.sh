2c9e7e3094e2b5c210aa8594a04495a458dde427
Razorr1996 <basa62.1996@gmail.com>
2017-04-16 00:03:06 +0300

    Script can use lib runtime

--- pack.sh
+++ pack.sh
@@ -2,13 +2,15 @@
 USAGE="Usage:\tpack.sh DIGIT"
 case "$1" in
     [1-5])
-        PA=pa"$1"
+        dir=pa"$1"
         mkdir target
-        mkdir target/$PA
-        rm target/$PA/*
-        cp src/"$1"lab.c src/functions.c headers/*.h headers/*/*.h target/$PA
+        mkdir target/$dir
+        rm target/$dir/*
+        cp src/"$1"lab.c src/functions.c target/$dir #source code
+        cp headers/*.h headers/*/*.h target/$dir #headers
+        cp library/*.so target/$dir #libs
         cd target
-        tar -cvzf $PA.tar.gz $PA
-        echo "all:\n\tclang -std=c99 -Wall -Werror -pedantic *.c" > $PA/Makefile;;
+        tar -cvzf $dir.tar.gz $dir
+        echo "all:\n\tclang -o ${1}lab -std=c99 -Wall -Werror -pedantic -L. -lruntime *.c" > $dir/Makefile;;
     *) echo "$USAGE";;
 esac
