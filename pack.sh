1ce09f2ee27d4d6dbfebc04889a1086f3a58d8ff
Razorr1996 <basa62.1996@gmail.com>
2017-04-15 22:10:29 +0300

    Add script func for all 5 labs

--- pack.sh
+++ pack.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-mkdir target
-mkdir target/pa1
-rm target/pa1/*
-cp src/main.c headers/*.h headers/*/*.h target/pa1
-cd target
-tar -cvzf pa1.tar.gz pa1
-echo "all:
-\tclang -std=c99 -Wall -Werror -pedantic *.c" > pa1/Makefile
+USAGE="Usage:\tpack.sh DIGIT"
+case "$1" in
+    [1-5])
+        PA=pa"$1"
+        mkdir target
+        mkdir target/$PA
+        rm target/$PA/*
+        cp src/"$1"lab.c src/functions.c headers/*.h headers/*/*.h target/$PA
+        cd target
+        tar -cvzf $PA.tar.gz $PA
+        echo "all:\n\tclang -std=c99 -Wall -Werror -pedantic *.c" > $PA/Makefile;;
+    *) echo "$USAGE";;
+esac
